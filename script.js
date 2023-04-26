5d92844212e97e5e679691594d9628ff95fda9ad
Gabriel00021 <123033653+Gabriel00021@users.noreply.github.com>
2023-04-26 20:15:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const tarefa = document.getElementById("tarefa")
+const aparece = document.getElementById("aparece")
+
+
+function result(){
+    let resultado = tarefa.value
+    console.log(resultado)
+    resultado
+}
